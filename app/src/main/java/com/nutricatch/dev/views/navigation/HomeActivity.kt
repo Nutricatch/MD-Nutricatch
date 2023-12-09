0b10cccad400cc1416ba6127b93c607b36e7f575
gojalifs <fajarsidik1999@gmail.com>
2023-12-09 17:20:20 +0700

    added animation on navigation

--- app/src/main/java/com/nutricatch/dev/views/navigation/HomeActivity.kt
+++ app/src/main/java/com/nutricatch/dev/views/navigation/HomeActivity.kt
@@ -2,24 +2,19 @@ package com.nutricatch.dev.views.navigation
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationBarView
 import com.nutricatch.dev.R
 import com.nutricatch.dev.databinding.ActivityHomeBinding
 import com.nutricatch.dev.views.navigation.camera.CameraActivity
-import com.nutricatch.dev.views.navigation.history.HistoryFragment
-import com.nutricatch.dev.views.navigation.home.HomeFragment
-import com.nutricatch.dev.views.navigation.profile.ProfileFragment
-import com.nutricatch.dev.views.navigation.recipes.RecipesFragment
 
-class HomeActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
+class HomeActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityHomeBinding
 
@@ -34,73 +29,34 @@ class HomeActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
         val navController = findNavController(R.id.nav_host_fragment_activity_home)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_history, R.id.navigation_setting
-            )
-        )
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.navigation_home,
+            R.id.navigation_recipe,
+            R.id.navigation_camera,
+            R.id.navigation_history,
+            R.id.navigation_setting
+        ).build()
 
         setSupportActionBar(binding.myToolbar)
         binding.myToolbar.visibility = View.GONE
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { _, navDestination, _ ->
+            binding.navView.isVisible = appBarConfiguration.isTopLevelDestination(navDestination)
+                .also { binding.fabScan.isVisible = it }
+        }
 
-//        binding.fabScan.setOnClickListener(this)
-
-        navView.setOnItemSelectedListener(this)
+        binding.fabScan.setOnClickListener(this)
 
         binding.navView.menu.getItem(2).isEnabled = false
 
     }
 
-//    override fun onClick(v: View) {
-//        when (v) {
-//            binding.fabScan -> {
-//                startActivity(Intent(this, CameraActivity::class.java))
-//            }
-//        }
-//    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        val fragmentManager = supportFragmentManager
-        val transaction = fragmentManager.beginTransaction()
-
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                transaction.replace(R.id.container, HomeFragment(), HomeFragment::class.java.name)
-            }
-
-            R.id.navigation_recipe -> {
-                transaction.replace(
-                    R.id.container,
-                    RecipesFragment(),
-                    RecipesFragment::class.java.name
-                )
-            }
-
-            R.id.navigation_camera -> {
-                /// todo handle navigate activity camera
+    override fun onClick(v: View) {
+        when (v) {
+            binding.fabScan -> {
                 startActivity(Intent(this, CameraActivity::class.java))
-                return false
-            }
-
-            R.id.navigation_history -> {
-                transaction.replace(
-                    R.id.container,
-                    HistoryFragment(),
-                    HistoryFragment::class.java.name
-                )
-            }
-
-            R.id.navigation_setting -> {
-                transaction.replace(
-                    R.id.container,
-                    ProfileFragment(),
-                    ProfileFragment::class.java.name
-                )
             }
         }
-        transaction.commit()
-        return true
     }
 }
--- app/src/main/java/com/nutricatch/dev/views/navigation/profile/ProfileFragment.kt
+++ app/src/main/java/com/nutricatch/dev/views/navigation/profile/ProfileFragment.kt
@@ -6,10 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import com.nutricatch.dev.R
 import com.nutricatch.dev.databinding.FragmentProfileBinding
-import com.nutricatch.dev.model.settingMenu
 
 class ProfileFragment : Fragment() {
 
@@ -21,30 +21,63 @@ class ProfileFragment : Fragment() {
     private val adapter: SettingMenuAdapter by lazy {
         SettingMenuAdapter(::onClickListener)
     }
+    private val viewModel by viewModels<ProfileViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val viewModel by viewModels<ProfileViewModel>()
 
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
+    }
 
-        val settingMenu = settingMenu
-        adapter.submitList(settingMenu)
-        binding.rvSettings.adapter = adapter
-        val layoutManager = LinearLayoutManager(requireContext())
-        binding.rvSettings.layoutManager = layoutManager
-        val itemDecoration = DividerItemDecoration(requireContext(), layoutManager.orientation)
-        binding.rvSettings.addItemDecoration(itemDecoration)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tileMyWeight.setOnClickListener {
+            it.findNavController().navigate(
+                R.id.action_navigation_setting_to_bodyDetailFragment
+            )
+        }
+        binding.tileLanguages.setOnClickListener {
+            it.findNavController().navigate(
+                R.id.action_navigation_setting_to_languageFragment
+            )
+        }
 
-        return root
+        binding.tileShare.setOnClickListener {
+            /// TODO implement share with intent explicit
+        }
+
+        binding.swTheme.setOnCheckedChangeListener { _, isChecked ->
+            changeTheme(isChecked)
+        }
+
+        binding.tileContact.setOnClickListener {
+            /// TODO implement intent to email
+        }
+
+        binding.tileHelp.setOnClickListener {
+            /// TODO implement navigate to help page
+        }
+
+        binding.tilePrivacy.setOnClickListener {
+            /// TODO implement navigate to privacy policy page
+        }
+
+    }
+
+    private fun changeTheme(isChecked: Boolean) {
+        /// TODO implement theme changing
     }
 
     private fun onClickListener(title: String) {
-
+        when (title.lowercase()) {
+            "my weight" -> {
+                Navigation.createNavigateOnClickListener(R.id.action_navigation_setting_to_bodyDetailFragment)
+            }
+        }
     }
 
     override fun onDestroyView() {
