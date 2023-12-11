2b25772161fb5edeafec77078531509bf251db36
Luminat3 <bskin132@gmail.com>
2023-12-12 00:41:27 +0700

    menambahkan chart pada homeview fragment

--- app/src/main/java/com/nutricatch/dev/views/navigation/home/HomeFragment.kt
+++ app/src/main/java/com/nutricatch/dev/views/navigation/home/HomeFragment.kt
@@ -8,6 +8,18 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.github.chartcore.common.ChartTypes
+import com.github.chartcore.common.Position
+import com.github.chartcore.common.TextAlign
+import com.github.chartcore.data.chart.ChartCoreModel
+import com.github.chartcore.data.chart.ChartData
+import com.github.chartcore.data.dataset.ChartNumberDataset
+import com.github.chartcore.data.option.ChartOptions
+import com.github.chartcore.data.option.elements.Elements
+import com.github.chartcore.data.option.elements.Line
+import com.github.chartcore.data.option.plugin.Plugin
+import com.github.chartcore.data.option.plugin.Title
+import com.github.chartcore.data.option.plugin.Tooltip
 import com.nutricatch.dev.R
 import com.nutricatch.dev.data.ResultState
 import com.nutricatch.dev.databinding.FragmentHomeBinding
@@ -34,6 +46,56 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val layoutManager = LinearLayoutManager(requireContext())
         binding.rvLatestPost.layoutManager = layoutManager
+
+        //ini demo untuk chart
+        val chartData = mapOf(
+            "Senin" to 1960.0,
+            "Selasa" to 1950.0,
+            "Rabu" to 1840.0,
+            "Kamis" to 1870.0,
+            "Jumat" to 1910.0,
+            "Sabtu" to 1920.0,
+            "Minggu" to 2010.0
+        )
+
+        val coreData = ChartData().addDataset(ChartNumberDataset().data(chartData.values.toList())
+            .label("Calories for Last 7 Days").offset(10)).labels(chartData.keys.toList())
+
+        val chartOptions = ChartOptions()
+            .plugin(
+                Plugin()
+                    .subtitle(
+                        Title()
+                            .display(true)
+                            .text("Subtitle example")
+                            .position(Position.BOTTOM)
+                    ).title(
+                        Title()
+                            .display(true)
+                            .text("Title")
+                            .position(Position.TOP)
+                            .align(TextAlign.CENTER)
+                            .color("red")
+                    )
+                    .tooltip(
+                        Tooltip(false)
+                    )
+            )
+            .elements(
+                Elements()
+                    .line(
+                        Line()
+                            .tension(0.5f)
+                    )
+            )
+
+        val chartModel = ChartCoreModel()
+            .type(ChartTypes.LINE)
+            .data(coreData)
+            .options(chartOptions)
+
+        binding.chartCore.draw(chartModel)
+
 
         /// nanti, ubah jadi observe ke viewmodel
         val user = "John Doe"
