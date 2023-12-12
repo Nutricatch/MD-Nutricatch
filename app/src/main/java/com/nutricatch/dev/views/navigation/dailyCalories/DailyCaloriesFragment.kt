9d2055daf3d5ec51d6aefa8d4b9e121ef706f5ac
Luminat3 <bskin132@gmail.com>
2023-12-12 17:04:25 +0700

    menambahkan daily calorie tracker

--- app/src/main/java/com/nutricatch/dev/views/navigation/dailyCalories/DailyCaloriesFragment.kt
+++ app/src/main/java/com/nutricatch/dev/views/navigation/dailyCalories/DailyCaloriesFragment.kt
@@ -6,55 +6,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.nutricatch.dev.R
+import com.nutricatch.dev.databinding.FragmentDailyCaloriesBinding
+import com.nutricatch.dev.databinding.FragmentHomeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DailyCaloriesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DailyCaloriesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentDailyCaloriesBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_daily_calories, container, false)
+        _binding = FragmentDailyCaloriesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val todayCalories = 1120
+        val goals = 2250
+        //TODO : Inisialisasi jumlah kalori dan progress bar ketika get kalori Harian
+        binding.tvCalories.text = todayCalories.toString()
+        binding.tvGoals.text = goals.toString()
+        binding.caloriesPercentage.progress = 48
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DailyCaloriesFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DailyCaloriesFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
     }
 }
