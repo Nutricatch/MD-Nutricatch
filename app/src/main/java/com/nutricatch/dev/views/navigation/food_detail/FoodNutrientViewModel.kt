f5921a4024199626c65796c5ac03417c160516ba
gojalifs <fajarsidik1999@gmail.com>
2023-12-20 23:24:48 +0700

    added add eating

--- app/src/main/java/com/nutricatch/dev/views/navigation/food_detail/FoodNutrientViewModel.kt
+++ app/src/main/java/com/nutricatch/dev/views/navigation/food_detail/FoodNutrientViewModel.kt
@@ -5,4 +5,14 @@ import com.nutricatch.dev.data.repository.FoodsRepository
 
 class FoodNutrientViewModel(private val repository: FoodsRepository) : ViewModel() {
     fun getFoodNutrient(name: String) = repository.getFoodByName(name)
+
+    fun saveEating(
+        calories: Double,
+        carbohydrates: Double,
+        fat: Double,
+        protein: Double,
+        salt: Double,
+        sugar: Double,
+        fiber: Double
+    ) = repository.addNewEating(calories, carbohydrates, fat, protein, salt, sugar, fiber)
 }
