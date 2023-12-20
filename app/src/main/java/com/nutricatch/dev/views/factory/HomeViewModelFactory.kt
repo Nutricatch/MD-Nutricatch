3617a0a4c37220659853c849c93384487cf38529
Luminat3 <bskin132@gmail.com>
2023-12-20 23:22:42 +0700

    -

--- app/src/main/java/com/nutricatch/dev/views/factory/HomeViewModelFactory.kt
+++ app/src/main/java/com/nutricatch/dev/views/factory/HomeViewModelFactory.kt
@@ -5,18 +5,21 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.nutricatch.dev.data.injection.Injection
 import com.nutricatch.dev.data.prefs.Preferences
+import com.nutricatch.dev.data.repository.DailyIntakeRepository
+import com.nutricatch.dev.data.repository.RecommendationRepository
 import com.nutricatch.dev.data.repository.UserRepository
 import com.nutricatch.dev.views.navigation.home.HomeViewModel
 
 class HomeViewModelFactory(
     private val userRepository: UserRepository,
-    private val preferences: Preferences
+    private val preferences: Preferences,
+    private val dailyIntakeRepository: DailyIntakeRepository
 ) :
     ViewModelProvider.NewInstanceFactory() {
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(HomeViewModel::class.java)) {
-            return HomeViewModel(userRepository, preferences) as T
+            return HomeViewModel(userRepository, preferences, dailyIntakeRepository) as T
         }
 
         throw IllegalArgumentException("Unknown viewModel class ${modelClass.name}")
@@ -30,7 +33,7 @@ class HomeViewModelFactory(
             INSTANCE ?: synchronized(this) {
                 INSTANCE ?: HomeViewModelFactory(
                     Injection.provideUserRepository(context),
-                    preferences
+                    preferences, Injection.provideDailyIntakeRepository(context)
                 )
             }.also { INSTANCE = it }
     }
